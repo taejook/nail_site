9326e7dae6f7265e2d735974b2274bdb153c5105
TaeJoo Kim <taejook1107@gmail.com>
2025-09-20 15:23:46 -0400

    fix: updated port

--- mycalendar_backend/server.js
+++ mycalendar_backend/server.js
@@ -184,7 +184,7 @@ app.post("/api/bookings", async (req, res) => {
 /** ===============================
  * Start server
  * =============================== */
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
